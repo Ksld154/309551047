fa6087635b08b5bf483afb194bb16b73cbc079b3
Alex Lin <lylin1997.cs09g@nctu.edu.tw>
2021-03-26 23:04:51 +0800

    [HW3-6] Real real failed test
    [Fix]
    Fix stupid mistake in assertEqual() again

--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -39,7 +39,7 @@ class PriorityQueueTest {
         for (int i = 0; i < random_array.length; i++) {
             result[i] = pq.poll();
         }
-        assertEquals(result, random_array);
+        assertEquals(correct_array, result);
     }
 
 
